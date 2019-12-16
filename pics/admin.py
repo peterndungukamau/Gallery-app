e86bc686ab48a970b61998a885c9fcea76917f61
peterndungukamau <peterndungukamau9@gmail.com>
2019-12-16 22:52:44 +0300

    filter location

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Image,location,Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    filter_horizontal = ('location',)
+
+admin.site.register(Image)
+admin.site.register(location)
+admin.site.register(Category,CategoryAdmin)
+
+
