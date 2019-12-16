c7493c5a56d02f4fa3d59a1b2ccd9bad3bcfe845
peterndungukamau <peterndungukamau9@gmail.com>
2019-12-16 22:45:59 +0300

    create new test

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image,location,Category
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        self.flower = Image(image_name = 'flower',image_desc ='lovely')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.flower,Image))
+
+    def test_save_method(self):
+        self.flower.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+        
