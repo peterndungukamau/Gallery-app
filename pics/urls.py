4b12932f302f778e632bb6cbf45b9ffaa9874c72
peterndungukamau <peterndungukamau9@gmail.com>
2019-12-16 22:45:18 +0300

    add category url route

--- pics/urls.py
+++ pics/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$',views.welcome,name = 'welcome'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^category/(\d+)',views.category,name ='category')
 ]
 
 if settings.DEBUG:
